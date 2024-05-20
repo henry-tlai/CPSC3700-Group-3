267e4fce4124ae3395cbdddfbfa0cbe43c29deec
ivannaandrea <166664131+ivannaandrea@users.noreply.github.com>
2024-05-20 01:14:51 -0700

    Update contactus.js

--- Sprint/JavaScript/contactus.js
+++ Sprint/JavaScript/contactus.js
@@ -2,7 +2,7 @@ document.getElementById('reason').addEventListener('change', function() {
     var showIf = document.getElementById('author');
     if (this.value === 'change restaurant information') {
         showIf.classList.remove('optional');
-    else if (this.value === 'contact author') {
+    } else if (this.value === 'contact author') {
         showIf.classList.remove('optional2');
     } else {
         showIf.classList.add('optional');
