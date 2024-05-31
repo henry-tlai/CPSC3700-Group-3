9eab43c8728b55451049a31b4dd6500713573e92
mbustos <mbustos@seattleu.edu>
2024-05-30 19:51:50 -0700

    Script.js

--- Sprint/JavaScript/script.js
+++ Sprint/JavaScript/script.js
@@ -17,7 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
             searchInput.focus();
         }
     }
-    
 
     const searchIcon = document.querySelector('.search-icon');
     console.log("Search icon found:", searchIcon);
